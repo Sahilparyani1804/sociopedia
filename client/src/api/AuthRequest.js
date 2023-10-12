9b0b9d7ef2b8b780a1be31d2a1bdc6d0cf9ee065
sahil paryani <sahilparyani313@gmail.com>
2023-10-12 15:03:30 +0530

    updated client

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({ baseURL: "https://socioapi.onrender.com" });
 
 export const logIn = (formData) => API.post("/auth/login", formData);
 export const signUp = (formData) => API.post("/auth/register", formData);
--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({ baseURL: "https://socioapi.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
